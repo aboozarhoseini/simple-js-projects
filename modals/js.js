0d4adee8031126ed86c21d21905bcb491a85355a
aboozar <aboozarh4@gmail.com>
2021-11-08 05:58:52 +0330

    1400-08-17 05:58 AM -----Modals----- add some refactor

--- modals/js.js
+++ modals/js.js
@@ -8,30 +8,40 @@ let main = document.querySelector('.main');
 let body = document.querySelector('body');
 let cover = document.querySelector('.cover');
 
-btn1.addEventListener('click', () => {
-    modalSlid.classList.add('modalMovDown');
-    main.classList.add( 'blur' );
+const blur = () => {
+    main.classList.add('blur');
     cover.classList.add('cover-visable');
-    
-} );
-btn2.addEventListener('click', () => {
-    modalScale.classList.add('modalScaleAnim');
-    main.classList.add( 'blur' );
-    cover.classList.add('cover-visable');
-} );
-btn3.addEventListener('click', () => {
-    modalOpacity.classList.add('modalOpacityAnim');
-    main.classList.add( 'blur' );
-    cover.classList.add('cover-visable');
-} );
+};
 
-
-cover.addEventListener( 'click', () => {
+const unBlur = () => {
     modalOpacity.classList.remove('modalOpacityAnim');
     modalScale.classList.remove('modalScaleAnim');
     modalSlid.classList.remove('modalMovDown');
     cover.classList.remove('cover-visable');
-    main.classList.remove( 'blur' );
-} );
+    main.classList.remove('blur');
+};
 
+btn1.addEventListener('click', () => {
+    modalSlid.classList.add('modalMovDown');
+    blur();
+});
 
+btn2.addEventListener('click', () => {
+    modalScale.classList.add('modalScaleAnim');
+    blur();
+});
+
+btn3.addEventListener('click', () => {
+    modalOpacity.classList.add('modalOpacityAnim');
+    blur();
+});
+
+cover.addEventListener('click', () => {
+    unBlur();
+});
+
+window.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+        unBlur();
+    }
+});
