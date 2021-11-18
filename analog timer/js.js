42b838015e3ab0000bdc83f2e853416bc7d5918f
aboozar <aboozarh4@gmail.com>
2021-11-18 09:48:53 +0330

    1400-08-27 09:48 AM ----- analog cloclk ----- add setting to Clock

--- analog timer/js.js
+++ analog timer/js.js
@@ -1,54 +1,106 @@
-const digitalTimer = document.querySelector('.bx .digitTime');
-const digitalHour = document.querySelector('.bx .digitTime .hour');
-const digitalMin = document.querySelector('.bx .digitTime .min');
-const digitalSec = document.querySelector('.bx .digitTime .sec');
+const digitalTimer = document.querySelector(".bx .digitTime");
+const digitalHour = document.querySelector(".bx .digitTime .hour");
+const digitalMin = document.querySelector(".bx .digitTime .min");
+const digitalSec = document.querySelector(".bx .digitTime .sec");
 
-const secTimer = document.querySelector('.bx .secTimer');
-const minTimer = document.querySelector('.bx .minTimer');
-const hourTimer = document.querySelector('.bx .hourTimer');
+const secTimer = document.querySelector(".bx .secTimer");
+const minTimer = document.querySelector(".bx .minTimer");
+const hourTimer = document.querySelector(".bx .hourTimer");
 
-const rd_manu = document.querySelector('#rd_manu');
-const rd_auto = document.querySelector('#rd_auto');
-const setting_inputs = document.querySelectorAll('.setting_input');
+const rd_manu = document.querySelector("#rd_manu");
+const rd_auto = document.querySelector("#rd_auto");
+const setting_inputs = document.querySelectorAll(".setting_input");
+
+const minInput = document.querySelector("#minInput");
+const hourInput = document.querySelector("#hourInput");
+const secInput = document.querySelector("#secInput");
+let flag = true;
+
+const btnSet = document.querySelector(".sectionSetBtn button");
+
+let setTimeoutForClock;
+
+class PersonalDate {
+  date = new Date();
+  minutes = this.date.getMinutes() + this.date.getSeconds() / 60;
+  hours = this.date.getHours() + this.date.getMinutes() / 60;
+  seconds = this.date.getSeconds();
+}
+
+
+
+
 
 function showTime() {
-    let myDate = new Date();
-    let editedHour = myDate.getHours() + myDate.getMinutes() / 60;
-    let editedMin = myDate.getMinutes() + myDate.getSeconds() / 60;
-    let sec = myDate.getSeconds() < 10 ? `0${ myDate.getSeconds() }` : myDate.getSeconds();
-    
-    for (let x = 0; x < rd_auto.getAttributeNames().length; x++) {
-        if (rd_auto.getAttributeNames()[x] === 'checked') {
-            setTimeout(showTime, 1000);
+  let p, hour, min, sec, editedMins, editedHours;
+  if (flag) {
+    p = new PersonalDate();
+    editedHours = p.hours;
+    editedMins = p.minutes;
+    hour = p.hours < 10 ? `0${Math.trunc(p.hours)}` : Math.trunc(p.hours);
+    min = p.minutes < 10 ? `0${Math.trunc(p.minutes)}` : Math.trunc(p.minutes);
+    sec = p.seconds < 10 ? `0${p.seconds}` : p.seconds;
+  } else {
+    hour = Number(hourInput.value);
+    min = Number(minInput.value);
+    sec = Number(secInput.value);
+    editedHours = hour;
+      editedMins = min;
+      
+  }
 
-            let hour = myDate.getHours() < 10 ? `0${Math.trunc(editedHour)}` : Math.trunc(editedHour);
-            let min = myDate.getMinutes() < 10 ? `0${Math.trunc(editedMin)}` : Math.trunc(editedMin);
+  digitalHour.textContent = `${hour} : `;
+  digitalMin.textContent = `${min} : `;
+  digitalSec.textContent = sec;
+  secTimer.style.transform = `translate(0,-50%) rotateZ(${sec * 6}deg)`;
+  minTimer.style.transform = ` translate(-3.5px,-50%)  rotateZ(${editedMins * 6}deg)`;
+  hourTimer.style.transform = `translate(-8px,-50%) rotateZ(${editedHours * 30}deg)`;
 
-            digitalHour.textContent = `${hour} : `;
-            digitalMin.textContent = `${min} : `;
-            digitalSec.textContent = sec;
-            secTimer.style.transform = `translate(0,-50%) rotateZ(${sec * 6}deg)`;
-            minTimer.style.transform = ` translate(-3.5px,-50%)  rotateZ(${editedMin * 6}deg)`;
-            hourTimer.style.transform = `translate(-8px,-50%) rotateZ(${editedHour * 30}deg)`;
-        } else {
-            // editedMin = editedHour = sec = 0;
-            // console.log('fsdfd');
+  // add valur for inputs
+  //   minInput.value = min;
+  //   hourInput.value = hour;
+  //   secInput.value = sec;
 
-        }
-    }
+  setTimeoutForClock = setTimeout(showTime, 1000);
 }
 
 showTime();
 
-rd_auto.addEventListener('change', () => {
-    setting_inputs.forEach(e => {
-        e.setAttribute('disabled', 'disabled');
-        showTime();
-    });
+const resetTime = () => {
+  digitalHour.textContent = 0;
+  digitalMin.textContent = 0;
+  digitalSec.textContent = 0;
+  secTimer.style.transform = `translate(0,-50%) rotateZ(${0}deg)`;
+  minTimer.style.transform = ` translate(-3.5px,-50%)  rotateZ(${0}deg)`;
+  hourTimer.style.transform = `translate(-8px,-50%) rotateZ(${0}deg)`;
+  minInput.value = 0;
+  hourInput.value = 0;
+  secInput.value = 0;
+  clearTimeout(setTimeoutForClock);
+};
+
+rd_auto.addEventListener("change", () => {
+  flag = true;
+  setting_inputs.forEach(e => {
+    e.setAttribute("disabled", "disabled");
+  });
+  showTime();
 });
 
-rd_manu.addEventListener('change', () => {
-    setting_inputs.forEach(e => {
-        e.removeAttribute('disabled');
-    });
+rd_manu.addEventListener("change", () => {
+  flag = false;
+  setting_inputs.forEach(e => {
+    e.removeAttribute("disabled");
+    e.setAttribute("placeholder", "0");
+  });
+  resetTime();
+});
+
+btnSet.addEventListener("click", () => {
+  if (minInput.value === "" || secInput.value === "" || hourInput.value === "" || minInput.value < 0 || secInput.value < 0 || hourInput.value < 0) {
+    alert("somethings is empty");
+  } else {
+    //   flag = true;
+    showTime();
+  }
 });
